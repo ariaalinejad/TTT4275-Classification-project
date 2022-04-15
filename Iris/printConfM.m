bc0a0063a5b70d61f3795a55fd443216566bb617
Thea Charlotte <theacharlotte@Theas-MacBook-Air.local>
2022-04-15 21:44:00 +0200

    jnkjbi

---
+++ Iris/printConfM.m
@@ -0,0 +1,14 @@
+ cmTrain = ConfmatrixTrain;
+ cmTest = ConfmatrixTest;
+
+ figure(1);
+ cm1 = confusionchart(cmTrain, {'Setosa','Versicolor','Verginica'});
+ cm1.Title = 'Iris Classification Training ';
+ cm1.RowSummary = 'row-normalized';%Displays the error rate
+ cm1.ColumnSummary = 'column-normalized';
+
+figure(2);
+ cm2 = confusionchart(cmTest, {'Setosa','Versicolor','Verginica'});
+ cm2.Title = 'Iris Classification Testing';
+ cm2.RowSummary = 'row-normalized';%Displays the error rate
+ cm2.ColumnSummary = 'column-normalized';
