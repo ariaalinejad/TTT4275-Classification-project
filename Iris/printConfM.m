357fe80a408eaaa021bd3ac5409dbb65de21ecc8
theacharlotte <85684520+theacharlotte@users.noreply.github.com>
2022-04-29 22:14:19 +0200

    Add files via upload

--- Iris/printConfM.m
+++ Iris/printConfM.m
@@ -6,9 +6,13 @@
  cm1.Title = 'Iris Classification Training ';
  cm1.RowSummary = 'row-normalized';%Displays the error rate
  cm1.ColumnSummary = 'column-normalized';
+ cm2.DiagonalColor = '#77AC30';
+
 
 figure(2);
  cm2 = confusionchart(cmTest, {'Setosa','Versicolor','Verginica'});
  cm2.Title = 'Iris Classification Testing';
  cm2.RowSummary = 'row-normalized';%Displays the error rate
  cm2.ColumnSummary = 'column-normalized';
+ cm2.DiagonalColor = '#77AC30';
+
