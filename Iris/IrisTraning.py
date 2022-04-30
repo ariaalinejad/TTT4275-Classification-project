2fd8c9e3fa5296148ba9cb47354029d9b24ee80e
ariaalinejad <79251626+ariaalinejad@users.noreply.github.com>
2022-04-30 09:14:03 +0200

    Update IrisTraning.py

--- Iris/IrisTraning.py
+++ Iris/IrisTraning.py
@@ -1,43 +1,58 @@
 import pandas as pd
 import numpy as np
-import wheel
 import matplotlib.pyplot as plt
 from tabulate import tabulate
 from scipy import io
 
-
-
-
 def main():
     #np.random.seed(1)
 
-    C = 3
-    D = 1
-    alpha  = 0.01
-    Ntrain = 30
-    Ntest  = 20
-    NtrainAll = Ntrain*3
-    NtestAll = Ntest*3
-    Niterations = 1000
-    ConfMatrixTrain = np.zeros([C,C])
-    ConfMatrixTest = np.zeros([C,C])
-    AllMSE = []
-    #W = 2*np.random.random((C,D+1))-1 # 3x5 matrix gives number between -1 and 1 
-    W = np.zeros((C,D+1))
+    indexRemoveFeature = [1]             #kjører først [] så [1] så [0,1] så [0,1,2]
+    C = 3                                #number of classes
+    D = 4 - len(indexRemoveFeature)      #number of features
+    alpha  = [0.01]   #[1,0.1,0.01,0.001,0.0001]  #0.01                        #step factor
+    Ntrain = 30                          #number of traning data for one class
+    Ntest  = 20                          #number of test data for one class
+    NtrainAll = Ntrain*C                 #number of traning data for all classes
+    NtestAll = Ntest*C                   #number of test data for all classes
+    Niterations = 2000                   #iterations of backpropagation
+    ConfMatrixTrain = np.zeros([C,C])    #initialize confusion matrix for train data 
+    ConfMatrixTest = np.zeros([C,C])     #initialize confusion matrix for test data 
+    AllMSETrain = [[0]*Niterations for i in range(len(alpha))]                          #array of all mean sqare errors
+    AllMSETest =  [[0]*Niterations for i in range(len(alpha))]                          #array of all mean sqare errors
+    #W = np.zeros((C,D+1))                #initialize waiting functions
+    #W = 2*np.random.random((C,D+1))-1   #3x5 matrix gives number between -1 and 1 
     #Predicted = np.zeros(90,3)
 
-    xTrain, xTest, tTrain, tTest = splitData('iris.data', Ntrain, [0,1,2])
+    xTrain, xTest, tTrain, tTest = splitData('iris.data', Ntrain, indexRemoveFeature)
+    #xTest, xTrain, tTest, tTrain = splitData('iris.data', Ntest, indexRemoveFeature)
+    #xTest, xTrain, tTest, tTrain = splitData('iris.data', Ntest)
     #xTrain, xTest, tTrain, tTest = splitData('iris.data', Ntrain)
+    
+    for j in range(len(alpha)):
+        W = np.zeros((C,D+1))   
+        for i in range(Niterations): # run 20 backpropagations
 
-    for i in range(Niterations): # run 20 backpropagations
+            zTrain, gTrain, MSETrain, GradMSETrain = findMSE(xTrain,tTrain, NtrainAll, C, W)
 
-        zTrain, gTrain, MSETrain, GradMSETrain = findMSE(xTrain,tTrain, NtrainAll, C, W)
+            W = W - alpha[j]*GradMSETrain
+            
+            AllMSETrain[j][i] = MSETrain
 
-        W = W - alpha*GradMSETrain
-        
-        AllMSE.append(MSETrain)
+            zTest, gTest, MSETest, GradMSETest = findMSE(xTest, tTest, NtestAll, C, W)
 
-    zTest, gTest, MSETest, GradMSETest = findMSE(xTest, tTest, NtestAll, C, W)
+            AllMSETest[j][i] = MSETest
+    for i in range(len(alpha)):
+        plt.plot(AllMSETrain[i], label=f"Training data \u03B1={alpha[i]}") 
+        plt.plot(AllMSETest [i], label=f"Testing  data \u03B1={alpha[i]}") 
+    plt.xlabel('iterations')
+    plt.ylabel('MSE')
+    plt.title('MSE for differenet \u03B1')
+
+    plt.legend()
+
+
+    
 
     # makes ConfMatrix for Training data
     for k in range(NtrainAll):
@@ -50,20 +65,19 @@ def main():
         ConfMatrixTest[np.argmax(tTest[k])][np.argmax(gTest[k])] += 1
     io.savemat('confmatrixTest.mat', {"ConfmatrixTest": ConfMatrixTest })
     
-
+    #printing and displaying of data
     print ('Weighting matrix: \n')
     for line in W:
         print ('\n ', '   '.join(map(str, line)))
 
-    print(f'Error rate(train): {round(findErrorRate(ConfMatrixTrain,NtrainAll, C),5)}% \n')
-    print(f'Error rate(train): {round(findErrorRate(ConfMatrixTest,NtrainAll, C),5)}% \n')
-    histogram('iris.data')
-    #plt.show()
-    
     #print(histogramList('iris.data', 0, 16))
-    #plt.plot(AllMSE) 
     #plt.hist(histogramList('iris.data', 0, 16))
-    #plt.show()
+
+    print(f'Error rate(train): {round(findErrorRate(ConfMatrixTrain,NtrainAll, C),5)}% \n')
+    print(f'Error rate(test):  {round(findErrorRate(ConfMatrixTest,NtrainAll, C),5)}% \n')
+    histogram('iris.data')
+    
+    plt.show()
     
     #print(f'Error rate(test): {round(MSE_test/NtestAll,5)}%')
 
@@ -78,8 +92,6 @@ def splitData(fileName, N, col = None):
     if (col != None):
         for i in col:
             df.drop(i, inplace=True, axis=1)
-    
-    #print(df)
     
     df_S    = df.iloc[   :50 ]
     df_Vc   = df.iloc[50 :100]
@@ -149,10 +161,10 @@ def histogram(fileName):
         axs[0].hist(classType[i][:50], bins=16, color = "mediumseagreen")
         axs[0].set_title('Iris-setosa')
         axs[0].set_ylabel('samples per bin')
-        axs[1].hist(classType[i][50:100], bins=16, color = "#77AC30")
+        axs[1].hist(classType[i][50:100], bins=16, color = "mediumaquamarine")
         axs[1].set_title('Iris-versicolor')
         axs[1].set_ylabel('samples per bin')
-        axs[2].hist(classType[i][100:150], bins=16, color = "mediumaquamarine")
+        axs[2].hist(classType[i][100:150], bins=16, color = "mediumturquoise")
         axs[2].set_title('Iris-virginica')
         axs[2].set_ylabel('samples per bin')
         axs[2].set_xlabel(className[i])
@@ -174,6 +186,3 @@ def histogram(fileName):
         print(overlapPrint[i], overlap[i])
 
 main()
-
-
-
