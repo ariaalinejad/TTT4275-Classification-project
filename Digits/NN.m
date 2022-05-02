90bbe1c71875cfb4c48e4c434315a06d66c5cdd8
theacharlotte <85684520+theacharlotte@users.noreply.github.com>
2022-05-02 14:21:32 +0200

    Update NN.m

--- Digits/NN.m
+++ Digits/NN.m
@@ -10,9 +10,7 @@ function [nearest, IndexTrainSet] = NN(trainSet,trainSetLabel, testSet, Nchunk, 
     for chunk = 1 : Nchunk
             startIterative = (chunk-1)*chunkSize+1;
             endIterative = chunk*chunkSize;
-            %W = trainSet(startIterative:endIterative,:); 
-            %P = testSet.'; 
-          
+         
             distances = dist(trainSet(startIterative:endIterative,:), testSet.');
     
             for i = 1 : NtestVal
